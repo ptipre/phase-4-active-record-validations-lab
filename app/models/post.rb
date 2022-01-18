5b2e1c511365d15500d863f1c279421208fd4cce
Prajay Tipre <prajaytipre@gmail.com>
2022-01-18 15:22:23 -0500

    commit tree 4ec6b9aa4c191091ab2ac39944155f66a0faf18f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait
+
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def is_clickbait
+            if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+        end
+    end
+
+
 end
